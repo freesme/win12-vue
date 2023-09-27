f83ef49ca54117c32d235fdb0b7820cb1fc9507b
Krane <freesme@163.com>
2023-09-27 11:19:11 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <ProgressBar :progress="progress" :load-time=1000 backColor="#C81D1DFF" :manual="false" :on-progress-complete="hel"/>
-  <button style="width: 100px" @click=addProgress>STEP</button>
+<!--  <ProgressBar :progress="progress" :load-time=1000 backColor="#C81D1DFF" :manual="false" :on-progress-complete="hel"/>-->
+<!--  <button style="width: 100px" @click=addProgress>STEP</button>-->
+<!--  <Panel></Panel>-->
+  <application></application>
 </template>
 
 
 <script setup>
-import ProgressBar from "./components/ProgressBar/ProgressBar.vue";
-import {ref, onMounted} from "vue";
+import {ref} from "vue";
+import Application from "./components/App/Application.vue";
+
 const progress = ref(90);
+
 function addProgress() {
   progress.value++
 }
 
 
-async function hel(){
+async function hel() {
   window.alert("Hel!")
   return "hel"
 }
