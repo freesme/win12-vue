928b6ce712b339cdabb8022b83e50aa5e645538e
Krane <freesme@163.com>
2024-09-09 10:45:38 +0800

    UPDATE FILE

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-<!--  <ProgressBar :progress="progress" :load-time=1000 backColor="#C81D1DFF" :manual="false" :on-progress-complete="hel"/>-->
-<!--  <button style="width: 100px" @click=addProgress>STEP</button>-->
+  <ProgressBar :progress="progress" :load-time=4000 backColor="#C81D1DFF" :manual="false" :on-progress-complete="hel"/>
+  <button style="width: 100px" @click=addProgress>STEP</button>
 <!--  <Panel></Panel>-->
-  <application></application>
+<!--  <application></application>-->
 </template>
 
 
 <script setup>
 import {ref} from "vue";
-import Application from "./components/App/Application.vue";
+import ProgressBar from "./components/ProgressBar/ProgressBar.vue";
+import Panel from "./components/Panel/Panel.vue";
 
-const progress = ref(90);
+const progress = ref(50);
 
 function addProgress() {
   progress.value++
